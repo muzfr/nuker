a6d47624a9478ba9151c459d659f8e024273d2c5
muz <108781584+muzfr@users.noreply.github.com>
2025-05-15 13:48:08 -0600

    Update COM (public).py

--- COM (public).py
+++ COM (public).py
@@ -13,7 +13,7 @@ def splash():
  ╚═════╝ ╚═════╝ ╚═╝     ╚═╝     ╚═════╝╚══════╝╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝   
 
                  ✦ COM CLIENT v1 — FULL SERVER FUCKER. ✦
-                   github.com/muzfr | @fanciers  | 2025
+                   github.com/muzfr | @imneverenough  | 2025
     """)
     time.sleep(2)
 
